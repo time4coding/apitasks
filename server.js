94b8c9fe9ebf2bef07837698f13ae2298acceb95
Ronald Davisson <ronaldadavisson@gmail.com>
2022-06-29 22:30:50 -0700

    v_120

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = __dirname + '/app/views/';
 const app = express();
-app.use(express.static(path));
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-const db = require("./app/models");
-db.sequelize.sync();
-app.get('/', function (req,res) {
-  res.sendFile(path + "index.html");
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to time4coding application." });
 });
-require("./app/routes/task.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
