daf5d1da5132fd4775c5ce694ff5059f8e9dd705
Ronald Davisson <ronaldadavisson@gmail.com>
2022-07-02 17:24:24 -0700

    v_501

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "dpg-capo44v6d9kiqtaa2920-a",
+    HOST: "dpg-capo44v6d9kiqtaa2920-a.oregon-postgres.render.com",
     USER: "admin",
     PASSWORD: "postgres",
     DB: "tasks_lmgp",
